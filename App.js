cf26ec05a4c9d7f99e4bdad33f884969c16106a1
Jorge Londoño <jmlz9427@gmail.com>
2023-04-03 18:29:23 -0500

    se crea la estructura del proyecto

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView, View,
+} from 'react-native';
+
+const App = () => {
+  
+
+  return (
+    <SafeAreaView style={{flex: 1}}>      
+    </SafeAreaView>
+  );
+};
+
+export default App;
